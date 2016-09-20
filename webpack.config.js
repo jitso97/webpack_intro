0f9d5bdf9b4e7511868a14c5efe4ae47d26a4a21
Jimmy Tso <jitso97@gmail.com>
2016-09-20 13:34:12 -0400

    changed output path for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/app.js",
   output: {
-    path: __dirname + "/js",
+    path: __dirname + "/bin",
     filename: "app.min.js"
   },
   plugins: debug ? [] : [
